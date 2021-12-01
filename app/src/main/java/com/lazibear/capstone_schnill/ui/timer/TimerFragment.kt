98eef485996f03c15424b86a4b9847126a315480
leonardchris1230 <68091366+leonardchris1230@users.noreply.github.com>
2021-12-01 10:56:13 +0700

    Working timer for now

--- app/src/main/java/com/lazibear/capstone_schnill/ui/timer/TimerFragment.kt
+++ app/src/main/java/com/lazibear/capstone_schnill/ui/timer/TimerFragment.kt
@@ -4,36 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.lazibear.capstone_schnill.databinding.FragmentTimerBinding
 
 class TimerFragment : Fragment() {
 
-    private lateinit var homeViewModel: TimerViewModel
-    private var _binding: FragmentTimerBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentTimerBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(TimerViewModel::class.java)
+        savedInstanceState: Bundle? ): View {
+       val timerViewModel = ViewModelProvider(this).get(TimerViewModel::class.java)
+
 
         _binding = FragmentTimerBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        val pomodoro = 25L
+        timerViewModel.setInitialTime(pomodoro)
+        timerViewModel.currentTimeString.observe(viewLifecycleOwner, { binding.textViewCountdown.text = it })
 
-        /*val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })*/
+        binding.fabStart.setOnClickListener {
+            timerViewModel.startTimer() }
+
+        binding.fabPause.setOnClickListener {}
+
+
+        binding.fabStop.setOnClickListener {
+            timerViewModel.resetTimer() }
+
         return root
     }
 
@@ -41,4 +43,5 @@ class TimerFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
 }
--- app/src/main/java/com/lazibear/capstone_schnill/ui/timer/TimerViewModel.kt
+++ app/src/main/java/com/lazibear/capstone_schnill/ui/timer/TimerViewModel.kt
@@ -1,13 +1,58 @@
 package com.lazibear.capstone_schnill.ui.timer
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class TimerViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private var timer: CountDownTimer? = null
+
+    private val initialTime = MutableLiveData<Long>()
+    private val currentTime = MutableLiveData<Long>()
+
+    // The String version of the current time (hh:mm:ss)
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time / 1000)
     }
-    val text: LiveData<String> = _text
+    fun getInitialTime() = initialTime.value
+
+    // Event which triggers the end of count down
+    private val _eventCountDownFinish = MutableLiveData<Boolean>()
+    val eventCountDownFinish: LiveData<Boolean> = _eventCountDownFinish
+
+    fun setInitialTime(minuteFocus: Long) {
+        val initialTimeMillis = minuteFocus * 60 * 1000
+        initialTime.value = initialTimeMillis
+        currentTime.value = initialTimeMillis
+
+        timer = object : CountDownTimer(initialTimeMillis, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                currentTime.value = millisUntilFinished
+            }
+
+            override fun onFinish() {
+                resetTimer()
+            }
+        }
+    }
+
+    fun startTimer() {
+        timer?.start()
+    }
+
+    fun resetTimer() {
+        timer?.cancel()
+        currentTime.value = initialTime.value
+        _eventCountDownFinish.value = true
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer?.cancel()
+    }
 }
