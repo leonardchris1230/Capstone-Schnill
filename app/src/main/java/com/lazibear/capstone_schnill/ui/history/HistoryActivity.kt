128cbc135597bc40d46941693a76c2883d002e73
LeonardChris <l.chrissan@gmail.com>
2021-12-15 14:16:58 +0700

    Adjusting UI on Timer Act and History Act

--- app/src/main/java/com/lazibear/capstone_schnill/ui/history/HistoryActivity.kt
+++ app/src/main/java/com/lazibear/capstone_schnill/ui/history/HistoryActivity.kt
@@ -1,13 +1,12 @@
 package com.lazibear.capstone_schnill.ui.history
 
+import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.lazibear.capstone_schnill.R
-import com.lazibear.capstone_schnill.data.HistoryDatabase
-import com.lazibear.capstone_schnill.data.HistoryRepository
 import com.lazibear.capstone_schnill.data.HistoryViewModelFactory
 import com.lazibear.capstone_schnill.databinding.ActivityHistoryBinding
 
@@ -15,21 +14,18 @@ class HistoryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHistoryBinding
     private lateinit var historyViewModel: HistoryViewModel
-    private lateinit var historyDatabase: HistoryDatabase
-    private lateinit var repository: HistoryRepository
     private lateinit var factory: HistoryViewModelFactory
     private lateinit var historyAdapter: HistoryAdapter
 
 
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setTheme(R.style.Theme_Capstone_Schnill)
         binding = ActivityHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
+        initMenu()
 
 
         factory = HistoryViewModelFactory.getInstance(this)
@@ -43,6 +39,14 @@ class HistoryActivity : AppCompatActivity() {
         historyViewModel.getAllHistory().observe(this, {
             historyAdapter.submitList(it)
         })
+
+
+    }
+
+    private fun initMenu() {
+        binding.historyToolbar.inflateMenu(R.menu.main_menu)
+        binding.historyToolbar.setNavigationIcon(R.drawable.ic_back_arrow)
+        binding.historyToolbar.setNavigationOnClickListener { onBackPressed() }
 
 
     }
